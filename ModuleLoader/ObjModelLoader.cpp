38a28f8181f9c680652c20907eb45845c2f63e34
Toby Smith <tobysmith568@hotmail.co.uk>
2019-11-08 22:15:20 +0000

    Read in texture coords and normals

--- ModuleLoader/ObjModelLoader.cpp
+++ ModuleLoader/ObjModelLoader.cpp
@@ -12,6 +12,8 @@ Model& ObjModelLoader::GetModel(std::string fileLocation)
 	string fileContent = fileUtils.ReadFile(fileLocation);
 
 	vector<GLfloat> vertices = ReadVertices(fileContent);
+	vector<GLfloat> textureCoords = ReadTextureCoords(fileContent);
+	vector<GLfloat> normals = ReadNormals(fileContent);
 	
 	static Model model;
 	return model;
@@ -31,4 +33,36 @@ std::vector<GLfloat> ObjModelLoader::ReadVertices(std::string data)
 	}
 
 	return vertices;
+}
+
+std::vector<GLfloat> ObjModelLoader::ReadTextureCoords(std::string data)
+{
+	vector<GLfloat> textureCoords = vector<GLfloat>();
+	const regex floatRegex("^vt (-?[0-9]+\\.[0-9]+) (-?[0-9]+\\.[0-9]+)");
+	smatch sm;
+
+	while (regex_search(data, sm, floatRegex)) {
+		for (int i = 1; i < sm.size(); i++) {
+			textureCoords.push_back(stof(sm[i]));
+		}
+		data = sm.suffix();
+	}
+
+	return textureCoords;
+}
+
+std::vector<GLfloat> ObjModelLoader::ReadNormals(std::string data)
+{
+	vector<GLfloat> normals = vector<GLfloat>();
+	const regex floatRegex("^vn (-?[01]\\.0+) (-?[01]\\.0+) (-?[01]\\.0+)");
+	smatch sm;
+
+	while (regex_search(data, sm, floatRegex)) {
+		for (int i = 1; i < sm.size(); i++) {
+			normals.push_back(stof(sm[i]));
+		}
+		data = sm.suffix();
+	}
+
+	return normals;
 }
--- ModuleLoader/ObjModelLoader.h
+++ ModuleLoader/ObjModelLoader.h
@@ -14,4 +14,6 @@ private:
 	FileUtils fileUtils;
 
 	std::vector<GLfloat> ReadVertices(std::string data);
+	std::vector<GLfloat> ReadTextureCoords(std::string data);
+	std::vector<GLfloat> ReadNormals(std::string data);
 };
